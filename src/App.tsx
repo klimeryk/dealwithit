097550c6e36208f40bfdec9a1a90207571bfb13e
Igor Klimer <github@gpg.klimer.eu>
2024-08-01 23:32:30 +0000

    Fix types
    Now that the proof of concept is working, let's clean it up a bit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import "jimp/browser/lib/jimp.js";
 import { BitmapImage, GifFrame, GifCodec, GifUtil } from "gifwrap";
+import type { Jimp } from "@jimp/core";
 import glassesImageUrl from "./assets/glasses.png";
 
 const { Jimp } = window;
 
-let glassesImage = null;
+let glassesImage: Jimp;
 
 function App() {
-  const [outputImage, setOutputImage] = useState(null);
+  const [outputImage, setOutputImage] = useState<string | null>(null);
   useEffect(() => {
     async function fetchData() {
       const originalGlassesImage = await Jimp.read(glassesImageUrl);
@@ -22,45 +23,47 @@ function App() {
     fetchData();
   }, []);
 
-  function handleGifLoad(event) {
-    setOutputImage(event.target.result);
-  }
+  function onInputImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedFiles = event.target.files;
+    if (!selectedFiles || selectedFiles.length === 0) {
+      // TODO: error handling
+      return;
+    }
 
-  async function handleInputImageLoad(event) {
-    const originalImage = await Jimp.read(event.target.result);
-    const image = originalImage.resize(256, 256, Jimp.RESIZE_BICUBIC);
-
-    const frames = [];
-    let jimpFrame = image.clone().blit(glassesImage, 0, 0);
-    let jimpBitmap = new BitmapImage(jimpFrame.bitmap);
-    GifUtil.quantizeDekker(jimpBitmap, 256);
-    let frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
-    frames.push(frame);
-    jimpFrame = image.clone().blit(glassesImage, 0, 20);
-    jimpBitmap = new BitmapImage(jimpFrame.bitmap);
-    GifUtil.quantizeDekker(jimpBitmap, 256);
-    frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
-    frames.push(frame);
-    jimpFrame = image.clone().blit(glassesImage, 0, 40);
-    jimpBitmap = new BitmapImage(jimpFrame.bitmap);
-    GifUtil.quantizeDekker(jimpBitmap, 256);
-    frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
-    frames.push(frame);
-
-    const codec = new GifCodec();
-    const gif = await codec.encodeGif(frames, { loops: 3 });
-
-    console.log("got gif!");
-
-    const fileReader = new FileReader();
-    fileReader.onload = handleGifLoad;
-    fileReader.readAsDataURL(new File([gif.buffer], "", { type: "image/png" }));
-  }
-
-  function onInputImageChange(event) {
     const reader = new FileReader();
-    reader.onload = handleInputImageLoad;
-    reader.readAsArrayBuffer(event.target.files[0]);
+    reader.onload = async () => {
+      const originalImage = await Jimp.read(reader.result as Buffer);
+      const image = originalImage.resize(256, 256, Jimp.RESIZE_BICUBIC);
+
+      const frames = [];
+      let jimpFrame = image.clone().blit(glassesImage, 0, 0);
+      let jimpBitmap = new BitmapImage(jimpFrame.bitmap);
+      GifUtil.quantizeDekker(jimpBitmap, 256);
+      let frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
+      frames.push(frame);
+      jimpFrame = image.clone().blit(glassesImage, 0, 20);
+      jimpBitmap = new BitmapImage(jimpFrame.bitmap);
+      GifUtil.quantizeDekker(jimpBitmap, 256);
+      frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
+      frames.push(frame);
+      jimpFrame = image.clone().blit(glassesImage, 0, 40);
+      jimpBitmap = new BitmapImage(jimpFrame.bitmap);
+      GifUtil.quantizeDekker(jimpBitmap, 256);
+      frame = new GifFrame(jimpBitmap, { delayCentisecs: 20 });
+      frames.push(frame);
+
+      const codec = new GifCodec();
+      const gif = await codec.encodeGif(frames, { loops: 3 });
+
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        setOutputImage(fileReader.result as string);
+      };
+      fileReader.readAsDataURL(
+        new File([gif.buffer], "", { type: "image/png" }),
+      );
+    };
+    reader.readAsArrayBuffer(selectedFiles[0]);
   }
 
   function renderOutputImage() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+declare global {
+  interface Window {
+    Jimp: typeof import("jimp");
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
