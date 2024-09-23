9ad42da4e69b69c8c69b16c4e1ab2d19507e088e
Igor Klimer <github@gpg.klimer.eu>
2024-09-23 16:49:50 +0000

    Improve spacing in sortable glasses list
    Squeezes out just a bit more pixles so the style select has a bit more
    room. Might need to rethink this whole approach, as no new options can
    be added anymore, but I'm sure I'll come up with something (like #22).

--- src/SortableGlassesItem.tsx
+++ src/SortableGlassesItem.tsx
@@ -85,10 +85,10 @@ function SortableGlassesItem({
     <li
       ref={setNodeRef}
       style={style}
-      className="py-2 px-2 text-sm font-medium text-gray-800 last:rounded-b-md dark:text-white hover:bg-slate-100 dark:hover:bg-slate-800"
+      className="py-2 pe-2 text-sm font-medium text-gray-800 last:rounded-b-md dark:text-white hover:bg-slate-100 dark:hover:bg-slate-800"
     >
-      <div className="flex gap-2 justify-between w-full">
-        <div className="flex gap-2">
+      <div className="flex justify-between w-full">
+        <div className="flex">
           <Button
             className="cursor-ns-resize"
             type="link"
@@ -97,42 +97,44 @@ function SortableGlassesItem({
             {...attributes}
             {...listeners}
           />
-          <Select
-            title="Style of glasses"
-            className="w-20"
-            data-id={glasses.id}
-            size="small"
-            defaultValue={glassesImageUrl}
-            onChange={handleStyleChange}
-            options={styleOptions}
-            popupMatchSelectWidth={false}
-          />
-          <Select
-            title="Direction from which the glasses should arrive in frame"
-            data-id={glasses.id}
-            size="small"
-            defaultValue="up"
-            onChange={handleDirectionChange}
-            options={directionOptions}
-          />
-          <Button
-            title="Flip glasses horizontally"
-            size="small"
-            type={glasses.flipHorizontally ? "primary" : "default"}
-            icon={<FlipH />}
-            data-id={glasses.id}
-            data-field="flipHorizontally"
-            onClick={onFlipChange}
-          />
-          <Button
-            title="Flip glasses vertically"
-            size="small"
-            type={glasses.flipVertically ? "primary" : "default"}
-            icon={<FlipV />}
-            data-id={glasses.id}
-            data-field="flipVertically"
-            onClick={onFlipChange}
-          />
+          <div className="flex gap-2">
+            <Select
+              title="Style of glasses"
+              className="w-24"
+              data-id={glasses.id}
+              size="small"
+              defaultValue={glassesImageUrl}
+              onChange={handleStyleChange}
+              options={styleOptions}
+              popupMatchSelectWidth={false}
+            />
+            <Select
+              title="Direction from which the glasses should arrive in frame"
+              data-id={glasses.id}
+              size="small"
+              defaultValue="up"
+              onChange={handleDirectionChange}
+              options={directionOptions}
+            />
+            <Button
+              title="Flip glasses horizontally"
+              size="small"
+              type={glasses.flipHorizontally ? "primary" : "default"}
+              icon={<FlipH />}
+              data-id={glasses.id}
+              data-field="flipHorizontally"
+              onClick={onFlipChange}
+            />
+            <Button
+              title="Flip glasses vertically"
+              size="small"
+              type={glasses.flipVertically ? "primary" : "default"}
+              icon={<FlipV />}
+              data-id={glasses.id}
+              data-field="flipVertically"
+              onClick={onFlipChange}
+            />
+          </div>
         </div>
         <div className="flex gap-2">
           <Button
